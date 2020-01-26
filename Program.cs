b741e47dbf2d7b1876b4bdf899d405c87fdb206a
Travis McCloskey <tmccloskey@usf.edu>
2020-01-26 17:59:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pause to display message
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World! I'm Travis :DD");
+            Console.ReadKey(true);
+        }
+    }
+}
